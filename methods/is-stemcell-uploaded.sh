cdc537c8b20c54b151c042bedf5f9aa7ffce966f
Valentin Zickner <valentin.zickner@mimacom.com>
2018-04-17 11:46:34 +0200

    Feature: stemcell comparison also accept newer releases of the stemcell.
    Signed-off-by: Valentin Zickner <valentin.zickner@mimacom.com>

--- methods/is-stemcell-uploaded.sh
+++ methods/is-stemcell-uploaded.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 . $PCFUP_DIR/methods/exec-om.sh
+. $PCFUP_DIR/methods/log.sh
 
 function isStemcellUploaded() {
   STEMCELL_VERSION=$1
+  STEMCELL_VERSION_PREFIX=$(echo $1 | awk -F. '{print $1}')
+  STEMCELL_VERSION_SUFFIX=$(echo $1 | awk -F. '{print $2}')
+
+  if [[ -z $STEMCELL_VERSION_SUFFIX ]]; then
+    STEMCELL_VERSION_SUFFIX=0
+  fi
+
+  logDebug "search for stemcell $STEMCELL_VERSION with prefix $STEMCELL_VERSION_PREFIX"
+
   diagnostic_report=$(
     execOM curl --silent --path "/api/v0/diagnostic_report"
   )
@@ -11,14 +21,25 @@ function isStemcellUploaded() {
   stemcell=$(
     echo $diagnostic_report |
     jq \
-      --arg version "$STEMCELL_VERSION" \
+      -r \
+      --arg version "$STEMCELL_VERSION_PREFIX" \
       --arg glob "$IAAS" \
-    '.stemcells[] | select(contains($version) and contains($glob))'
+    '.stemcells[] | select(contains($version) and contains($glob))' |
+    sort -n -r |
+    head -1
   )
 
   if [[ -z "$stemcell" ]]; then
+    logDebug "no stemcell is available, exit"
     return 1
-  else
+  fi
+
+  stemcell_suffix_available=$(echo $stemcell | sed -E "s/.*-$STEMCELL_VERSION_PREFIX\.([0-9]+)-.*/\\1/")
+
+  if [[ $stemcell_suffix_available -ge $STEMCELL_VERSION_SUFFIX ]]; then
+    logDebug "available stemcell: $stemcell"
     return 0
+  else
+    return 1
   fi
 }
